6f8d70260dcb2a2bb4718a793920d736f30bb5d2
cheandgit <104068698+cheandgit@users.noreply.github.com>
2026-01-20 19:18:37 +0500

    Update CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import os
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)   # Разрешает запросы с любых доменов
 
 # Для Flask 3.0+
 app.json.ensure_ascii = False  # РАБОТАЕТ!

1b892d495e9dc2c320070c0aabcbc7d91f19f18e
cheandgit <104068698+cheandgit@users.noreply.github.com>
2026-03-04 15:49:19 +0500

    Update app.py - new student list

--- app.py
+++ app.py
@@ -8,9 +8,36 @@ app.json.ensure_ascii = False
 CORS(app)  # Разрешает запросы с любых доменов
 
 # Используем список для хранения студентов
+# students = [
+#    {"id": 1, "name": "Иван Иванов", "group": "ПИ-101"},
+#    {"id": 2, "name": "Мария Петрова", "group": "ИС-202"}
+# ]
 students = [
-    {"id": 1, "name": "Иван Иванов", "group": "ПИ-101"},
-    {"id": 2, "name": "Мария Петрова", "group": "ИС-202"}
+    {'id': 1, 'name': 'Анжелика Станиславовна Гаврилова', 'group': 'ПИ-101', 'email': 'adam_71@example.com'},
+    {'id': 2, 'name': 'Ермолай Фокич Медведев', 'group': 'ВТ-505', 'email': 'foka_03@example.net'},
+    {'id': 3, 'name': 'Лаврентий Иларионович Фадеев', 'group': 'ВТ-505', 'email': 'qmironov@example.net'},
+    {'id': 4, 'name': 'Гришина Евпраксия Макаровна', 'group': 'ПИ-101', 'email': 'fprohorov@example.org'},
+    {'id': 5, 'name': 'Гуляева Ульяна Федоровна', 'group': 'ПИ-101', 'email': 'filatovipat@example.org'},
+    {'id': 6, 'name': 'Симон Антипович Иванов', 'group': 'ИТ-404', 'email': 'timofeevavera@example.net'},
+    {'id': 7, 'name': 'Белова Милица Яковлевна', 'group': 'ПИ-101', 'email': 'oktjabrinagureva@example.com'},
+    {'id': 8, 'name': 'Анна Николаевна Калашникова', 'group': 'ПИ-101', 'email': 'leonti82@example.net'},
+    {'id': 9, 'name': 'Князева Фаина Владимировна', 'group': 'ИС-202', 'email': 'leon_20@example.com'},
+    {'id': 10, 'name': 'Зинаида Богдановна Рожкова', 'group': 'ИС-202', 'email': 'julian_1977@example.org'},
+    {'id': 8, 'name': 'Анна Николаевна Калашникова', 'group': 'ПИ-101', 'email': 'leonti82@example.net'},
+    {'id': 9, 'name': 'Князева Фаина Владимировна', 'group': 'ИС-202', 'email': 'leon_20@example.com'},
+    {'id': 8, 'name': 'Анна Николаевна Калашникова', 'group': 'ПИ-101', 'email': 'leonti82@example.net'},
+    {'id': 9, 'name': 'Князева Фаина Владимировна', 'group': 'ИС-202', 'email': 'leon_20@example.com'},
+    {'id': 10, 'name': 'Зинаида Богдановна Рожкова', 'group': 'ИС-202', 'email': 'julian_1977@example.org'},
+    {'id': 11, 'name': 'Игнатий Гурьевич Королев', 'group': 'ВТ-505', 'email': 'solomonprohorov@example.com'},
+    {'id': 12, 'name': 'Беспалов Арефий Тихонович', 'group': 'ИТ-404', 'email': 'agafjabelozerova@example.org'},
+    {'id': 13, 'name': 'Радован Викентьевич Козлов', 'group': 'ПИ-101', 'email': 'anisimgromov@example.org'},
+    {'id': 14, 'name': 'Прохоров Лучезар Викторович', 'group': 'ИТ-404', 'email': 'alevtina_65@example.net'},
+    {'id': 15, 'name': 'Полина Васильевна Калинина', 'group': 'ИТ-404', 'email': 'marija2025@example.net'},
+    {'id': 16, 'name': 'Наина Филипповна Назарова', 'group': 'ИТ-404', 'email': 'efremfedotov@example.com'},
+    {'id': 17, 'name': 'Артемьева Варвара Святославовна', 'group': 'ВТ-505', 'email': 'kuzma_1983@example.org'},
+    {'id': 18, 'name': 'Феврония Никифоровна Горбунова', 'group': 'ИТ-404', 'email': 'jsamsonova@example.net'},
+    {'id': 19, 'name': 'Иванов Соломон Харлампович', 'group': 'ПИ-101', 'email': 'nsemenov@example.org'},
+    {'id': 20, 'name': 'Любовь Тарасовна Евсеева', 'group': 'ВТ-505', 'email': 'zhanna1979@example.com'},
 ]
 
 # Получаем ключ из переменных окружения
